b11961e2eec2922c18a40f57fab6810f5c531f95
idini <maurizio.idini@gmail.com>
2021-04-06 23:38:36 +0200

    fix identation

--- GenderAgePoc/GenderAgeEstimatorOffline.py
+++ GenderAgePoc/GenderAgeEstimatorOffline.py
@@ -3,23 +3,23 @@ from facelib import special_draw
 import cv2
 
 class GenderAgeEstimatorOffline:
-    def __init__(self, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
-        print('loading ...') 
-        self.face_detector = FaceDetector(device=device)
-        self.age_gender_detector = AgeGenderEstimator(device=device)
+	def __init__(self, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
+		print('loading ...') 
+		self.face_detector = FaceDetector(device=device)
+		self.age_gender_detector = AgeGenderEstimator(device=device)
 
-    def predict(self, image, show=False):
+	def predict(self, image, show=False):
 		faces, boxes, scores, landmarks = self.face_detector.detect_align(image)
 		genders, ages = self.age_gender_detector.detect(faces)
 
-        if draw:
-        	if len(faces.shape) > 1:
-        		frame = image.copy()
-	            for i, b in enumerate(boxes):
-	            	# create new image and draw on it
-	                special_draw(frame, b, landmarks[i], name=genders[i]+' '+str(ages[i]))
-	        cv2.imshow('frame', frame)
-	        if cv2.waitKey(1) == ord('q'):
+		if draw:
+			if len(faces.shape) > 1:
+				frame = image.copy()
+				for i, b in enumerate(boxes):
+					# create new image and draw on it
+					special_draw(frame, b, landmarks[i], name=genders[i]+' '+str(ages[i]))
+			cv2.imshow('frame', frame)
+			if cv2.waitKey(1) == ord('q'):
 				cv2.destroyAllWindows()
 
 		return genders, ages
